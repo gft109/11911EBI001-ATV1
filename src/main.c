e6cefab9f12da4ccae8afcb77c2b27f40099e999
Gustavo <gft1090gmail.com>
2022-06-12 16:41:26 -0300

    Vetores de interrupção e exceções

--- src/main.c
+++ src/main.c
@@ -15,8 +15,8 @@
                                             register */
 #define STM32_GPIO_MODER_OFFSET 0x0000 /* GPIO port mode register */
 #define STM32_GPIO_OTYPER_OFFSET 0x0004 /* GPIO port output type register */
-#define STM32_GPIO_PUPDR_OFFSET 0x000c /* GPIO port pull-up/pull-down
-                                            register */
+#define STM32_GPIO_PUPDR_OFFSET 0x000c /* GPIO port pull-up/pull-down register */
+#define STM32_GPIO_ODR_OFFSET   0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET 0x0018 /* GPIO port bit set/reset register */
 
 /* Register Addresses *******************************************************/
@@ -25,6 +25,7 @@
 #define STM32_GPIOC_MODER (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOC_OTYPER (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
 #define STM32_GPIOC_PUPDR (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_ODR   (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOC_BSRR (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
 
 /* AHB1 Peripheral Clock enable register */
@@ -37,32 +38,48 @@
 #define GPIO_MODER_ALT (2) /* Alternate mode */
 #define GPIO_MODER_ANALOG (3) /* Analog mode */
 
-#define GPIO_MODER13_SHIFT (26)
-#define GPIO_MODER13_MASK (3 << GPIO_MODER13_SHIFT)
+#define GPIO_MODER_SHIFT(n)     (n << 1)
+#define GPIO_MODER_MASK(n)      (3 << GPIO_MODER_SHIFT(n))
 
 
 /* GPIO port output type register */
 #define GPIO_OTYPER_PP (0) /* 0=Output push-pull */
 #define GPIO_OTYPER_OD (1) /* 1=Output open-drain */
-#define GPIO_OT13_SHIFT (13)
-#define GPIO_OT13_MASK (1 << GPIO_OT13_SHIFT)
+
+#define GPIO_OT_SHIFT(n)        (n)
+#define GPIO_OT_MASK(n)         (1 << GPIO_OT_SHIFT(n))
 
 
 /* GPIO port pull-up/pull-down register */
 #define GPIO_PUPDR_NONE (0) /* No pull-up, pull-down */
 #define GPIO_PUPDR_PULLUP (1) /* Pull-up */
 #define GPIO_PUPDR_PULLDOWN (2) /* Pull-down */
-#define GPIO_PUPDR13_SHIFT (26)
-#define GPIO_PUPDR13_MASK (3 << GPIO_PUPDR13_SHIFT)
+
+#define GPIO_PUPDR_SHIFT(n)     (n << 1)
+#define GPIO_PUPDR_MASK(n)      (3 << (n << 1))
 
 
 /* GPIO port bit set/reset register */
+
 #define GPIO_BSRR_SET(n) (1 << (n))
 #define GPIO_BSRR_RST(n) (1 << (n + 16))
 
 
 //static const char fw_version[] = {'V', '1', '.', '0'};
-static uint32_t led_status;
+
+
+void delay(uint32_t time)
+{
+  uint32_t i;
+
+  for (i = 0; i <time; i++)
+    {
+      asm("nop");
+    }
+}
+
+
+#define LED_DELAY  50000
 
 int main(int argc, char *argv[])
 {
@@ -96,17 +113,18 @@ int main(int argc, char *argv[])
     reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
     *pGPIOC_PUPDR = reg;
 
-    int LED_DELAY =100;
     while(1)
     {
+        // liga led
         *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-        led_status = 0;
-        for(uint32_t i = 0; i < LED_DELAY; i++);
-        *pGPIOC_BSRR = GPIO_BSRR_RST(13);
-        led_status = 1;
-        for(uint32_t i = 0; i < LED_DELAY; i++);
+        delay(LED_DELAY);
+
+        // desliga led
+        *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+        delay(LED_DELAY);
  
     }
+
     /* Nao deveria chegar aqui */
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }
